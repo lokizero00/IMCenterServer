3b76e8ada8382230b1e49e2017e9d06276ed2b41
lokizero00 <lokizero0@live.com>
2018-07-25 20:03:07 +0800

    提交

--- src/main/java/com/loki/server/service/impl/RoleServiceImpl.java
+++ src/main/java/com/loki/server/service/impl/RoleServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.loki.server.dao.RoleDao;
@@ -20,6 +23,8 @@ import com.loki.server.utils.ResultCodeEnums;
 import com.loki.server.utils.ServiceException;
 import com.loki.server.vo.RoleVO;
 
+@Service
+@Transactional
 public class RoleServiceImpl extends BaseService implements RoleService{
 	@Resource 
 	RoleDao roleDao;
